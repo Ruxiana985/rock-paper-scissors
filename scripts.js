7ffd3f4fabc3da23903947afb32b6750c3618c64
Ruxiana985 <odinproject.ruxiana985@gmail.com>
2024-09-16 02:13:12 -1100

    process of editing the css part

--- scripts.js
+++ scripts.js
@@ -3,78 +3,110 @@ let paper = document.querySelector('.paper');
 let scissor = document.querySelector('.scissors');  
 let point1 =  document.querySelector('.user-point');
 let point2 =  document.querySelector('.comp-point');
-let user=document.querySelector('.user');
-let comp= document.querySelector('.comp');
 let start=document.querySelector('.start');
-let count1=0;
-let count2=0;
+let trail=document.querySelector('.trail');
+let count1 = 0;
+let count2 = 0;
+let round=0;
 
 
+function disableButtons() {
+  rock.disabled = true;
+  paper.disabled = true;
+  scissor.disabled = true;
+}
 
 
-rock.onclick = function rockop() {
+function enableButtons() {
+  rock.disabled = false;
+  paper.disabled = false;
+  scissor.disabled = false;
+}
+  
 
+rock.onclick = function() {
   let compchoice = Math.floor(Math.random() * 3);
   switch (compchoice) {
-    case 0:
-      count1=0;
-      count2=0;
+    case 0: // computer chooses rock
+      // Tie, no one scores
       break;
-    case 1:
-      count1=0;
-      count2++;
+    case 1: // computer chooses paper
+      count2++; // Computer wins
       break;
-    case 2:
-      count1++;
-      count2=0;
+    case 2: // computer chooses scissors
+      count1++; // User wins
       break;
-    
   }
-  
-  point1.textContent=`ROUND 1 POINT: ${count1}`
-  point2.textContent=`ROUND 1 POINT: ${count2}`
-  
+  round++;
+  updateScore();
+  counter();
 };
 
-paper.onclick = function paperop() {
+
+paper.onclick = function() {
   let compchoice = Math.floor(Math.random() * 3);
   switch (compchoice) {
-    case 0:
-      count1++;
-      count2=0;
+    case 0: // computer chooses rock
+      count1++; // User wins
       break;
-    case 1:
-     count1=0;
-      count2=0;
+    case 1: // computer chooses paper
+      // Tie, no one scores
       break;
-    case 2:
-      count1=0;
-      count2++;
+    case 2: // computer chooses scissors
+      count2++; // Computer wins
       break;
-    
-  
   }
-  point1.textContent=`ROUND 1 POINT: ${count1}`
-  point2.textContent=`ROUND 1 POINT: ${count2}`
+  updateScore();
+  round++;
+  counter();
 };
 
-scissor.onclick = function sciop() {
+
+scissor.onclick = function() {
   let compchoice = Math.floor(Math.random() * 3);
   switch (compchoice) {
-    case 0:
-      count1=0;
-      count2++;
+    case 0: // computer chooses rock
+      count2++; // Computer wins
       break;
-    case 1:
-      count1++;
-      count2=0;
+    case 1: // computer chooses paper
+      count1++; // User wins
       break;
-    case 2:
-      count1=0;
-      count2=0;
+    case 2: // computer chooses scissors
+      // Tie, no one scores
       break;
-    
   }
-  point1.textContent=`ROUND 1 POINT: ${count}`
-  point2.textContent=`ROUND 1 POINT: ${count2}`
+  updateScore();
+  round++;
+  counter();
 };
+
+start.onclick= function starting(){
+  count1 = 0;
+  count2 = 0;
+  round = 0;
+  trail.textContent = "Starting a new game!";
+  updateScore();
+  enableButtons();
+}
+  function counter(){
+    if (round == 5) {
+      disableButtons(); 
+    if (count1 > count2) {
+      trail.textContent = "Game over! You won! Click Start to play again.";
+    } else if (count1 < count2) {
+      trail.textContent = "Game over! Computer won! Click Start to play again.";
+    } else {
+      trail.textContent = "It's a tie! Click Start to play again.";
+    }
+    start.disabled = false;
+  }
+      
+}
+  
+
+
+
+function updateScore() {
+  point1.textContent = `User Score: ${count1}`;
+  point2.textContent = `Computer Score: ${count2}`;
+}
